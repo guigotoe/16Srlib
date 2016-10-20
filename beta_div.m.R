c6c97df733ef6569bdbe8f7e56cf96a54bbf001a
GuillermoTorres <guigotoe@gmail.com>
2016-10-20 12:17:15 +0200

    last update

--- beta_div.m.R
+++ beta_div.m.R
@@ -36,23 +36,23 @@ get_script_path <- function() {
 }
 script.basename <- dirname(get_script_path())
 toolbox <- paste(sep="/", script.basename, "toolbox.R")
-toolbox <- '/home/torres/Documents/Projects/Metagenome/r_scripts/16Srlib/toolbox.R'
-#toolbox <- "/Users/guillermotorres/Documents/Proyectos/Doctorado/16Srlib/toolbox.R"
+#toolbox <- '/home/torres/Documents/Projects/Metagenome/r_scripts/16Srlib/toolbox.R'
+toolbox <- "/Users/guillermotorres/Documents/Proyectos/Doctorado/16Srlib/toolbox.R"
 source(toolbox)
 packages(c("metagenomeSeq","reshape2","vegan","ggplot2","optparse"))
 
 ## Options ##
-p <- '/home/torres/ikmb_storage/projects/16Srlib_test/'
-#p <- '/Users/guillermotorres/Documents/Proyectos/Doctorado/16Srlib_test/'
+#p <- '/home/torres/ikmb_storage/projects/16Srlib_test/'
+p <- '/Users/guillermotorres/Documents/Proyectos/Doctorado/16Srlib_test/'
 
 option_list <- list(
   make_option(c("-i","--data"),type="character",default=paste(p,'results/dataF.rds',sep=''),
               help="Path to input rds file"),
   make_option(c("-o","--out"),type="character",default=paste(p,'results/',sep=''),
              help="Path to output directory [default %default]"),
-  make_option(c("-e","--exploratory"),action="store_true",default=FALSE,
+  make_option(c("-e","--exploratory"),action="store_true",
               help="Perform exploratory analysis"),
-  make_option(c("-m","--model"),action="store_true",default=FALSE,
+  make_option(c("-m","--model"),action="store_true",
               help="Build constrained model based on AIC selection criterion"),
   make_option(c("-am","--assess_model"),type="character",default='',#NULL,
               help="Model's terms assessed by permutation tests; for new model: vars,separated,by,comma"),
@@ -92,7 +92,7 @@ c <- pData(dataTrimed)[, sapply(pData(dataTrimed), is.factor)]
 ###### end ######
 
 if (opt$exploratory==T){
-  pdf(paste(opt$out,"e_data_filtering.pdf",sep=''),width=8, height=5)
+  pdf(paste(opt$out,"e01_data_filtering.pdf",sep=''),width=8, height=5)
   plot(csnorm)
   abline(h=quantile(csnorm,probs=opt$filter))
   dev.off()
@@ -103,20 +103,20 @@ if (opt$exploratory==T){
   #    between ordination and environmental variable.
   
   nmds <- metaMDS(dfc,trace=F)
-  pdf(paste(opt$out,"e_NMDS.pdf",sep=''),width=8, height=5)
+  pdf(paste(opt$out,"e02_NMDS.pdf",sep=''),width=8, height=5)
   ordiplot(nmds,type="t",display="sites")
   dev.off()
   ef <- envfit(nmds,q,permu=999)
   sink(file=paste(opt$out,"e_Envfitting2NMDS.txt",sep='')) 
   ef 
   sink(NULL) 
-  pdf(paste(opt$out,"e_EnvNMDS.pdf",sep=''),width=8, height=5)
+  pdf(paste(opt$out,"e03_EnvNMDS.pdf",sep=''),width=8, height=5)
   plot(nmds,display="sites")
   plot(ef,p.max=0.05)
   dev.off()
   
   pca <- rda(dfc,scale=TRUE)
-  pdf(paste(opt$out,"e_PCA.pdf",sep=''),width=8, height=5)
+  pdf(paste(opt$out,"e04_PCA.pdf",sep=''),width=8, height=5)
   plot(pca)
   text(pca,display="sites")
   dev.off()
@@ -124,14 +124,14 @@ if (opt$exploratory==T){
   sink(file=paste(opt$out,"e_Envfitting2PCA.txt",sep='')) 
   ef 
   sink(NULL) 
-  pdf(paste(opt$out,"e_EnvPCA.pdf",sep=''),width=8, height=5)
+  pdf(paste(opt$out,"e05_EnvPCA.pdf",sep=''),width=8, height=5)
   plot(pca,display="sites",xlab=paste("CA1:",round(pca$CA$eig[1]/sum(pca$CA$eig),2)),
        ylab=paste("CA2:",round(pca$CA$eig[2]/sum(pca$CA$eig),2)))
   plot(ef,p.max=0.05)
   dev.off()
   
   ca <- cca(dfc)
-  pdf(paste(opt$out,"e_CA.pdf",sep=''),width=8, height=5)
+  pdf(paste(opt$out,"e06_CA.pdf",sep=''),width=8, height=5)
   plot(ca)
   text(ca,display="sites")
   dev.off()
@@ -139,14 +139,14 @@ if (opt$exploratory==T){
   sink(file=paste(opt$out,"e_Envfitting2CA.txt",sep='')) 
   ef 
   sink(NULL) 
-  pdf(paste(opt$out,"e_EnvCA.pdf",sep=''),width=8, height=5)
+  pdf(paste(opt$out,"e07_EnvCA.pdf",sep=''),width=8, height=5)
   plot(ca,display="sites",xlab=paste("CA1:",round(ca$CA$eig[1]/sum(ca$CA$eig),2)),
        ylab=paste("CA2:",round(ca$CA$eig[2]/sum(ca$CA$eig),2)))
   plot(ef,p.max=0.05)
   dev.off()
 
   dca <- decorana(dfc)
-  pdf(paste(opt$out,"e_CA.pdf",sep=''),width=8, height=5)
+  pdf(paste(opt$out,"e08_DCA.pdf",sep=''),width=8, height=5)
   plot(dca)
   text(dca,display="sites")
   dev.off()
@@ -154,7 +154,7 @@ if (opt$exploratory==T){
   sink(file=paste(opt$out,"e_Envfitting2DCA.txt",sep='')) 
   ef 
   sink(NULL) 
-  pdf(paste(opt$out,"e_EnvDCA.pdf",sep=''),width=8, height=5)
+  pdf(paste(opt$out,"e09_EnvDCA.pdf",sep=''),width=8, height=5)
   plot(dca,display="sites")
   plot(ef,p.max=0.05)
   dev.off()
@@ -174,23 +174,23 @@ if (opt$model==T){
   mod1 <- cca(dfc ~ .,q)
   message("Building a model...")
   #modrev <- step(mod1,scope=list(lower=formula(mod0),upper=formula(mod1)),trace=0) 
-  sink(file=paste(opt$out,"m_fittingmodel.txt",sep='')) 
+  sink(file=paste(opt$out,"m01_fittingmodel.txt",sep='')) 
   mod <- step(mod0,scope=formula(mod1),test="perm")
   
-  print(" Variance Inflation Factors ")
+  print("\n Variance Inflation Factors ")
   vif.cca(mod)
   
-  print(" Permutation test - significance of model ")
+  print("\n Permutation test - significance of model ")
   anova(mod)
   
-  print(" Permutation test - significance of the terms ")
+  print("\n Permutation test - significance of the terms ")
   anova(mod,by="terms",perm=1000)
   sink(NULL) 
   
   terms <- attr(mod$terms,"term.labels")
   y <- vif.cca(mod)
   Rterms <- c()
-  message("Builded-model correction...")
+  message("Builded-model correction...\n")
   while(length(terms)>0){
     Rterms <- c(Rterms,names(which(y==max(y))))
     terms <- terms[-which(y==max(y))]
@@ -203,25 +203,25 @@ if (opt$model==T){
     if (a$`Pr(>F)`[length(a$`Pr(>F)`)-1]>0.1){Rterms <- Rterms[-length(Rterms)]}
   }
   modc <- cca(as.formula(paste("dfc~",paste(Rterms,collapse='+'))),q)
-  sink(file=paste(opt$out,"m_fittedmodel_corrected.txt",sep=''))
+  sink(file=paste(opt$out,"m02_fittedmodel_corrected.txt",sep=''))
   
-  print(" Variance Inflation Factors ")
+  print("\n Variance Inflation Factors ")
   vif.cca(modc)
   
-  print(" Permutation test - significance of model ")
+  print("\n Permutation test - significance of model ")
   anova(modc)
   
-  print(" Permutation test - significance of the terms ")
+  print("\n Permutation test - significance of the terms ")
   anova(modc,by="terms",perm=1000)
 
-  print(" Permutation test - marginal effects ")
+  print("\n Permutation test - marginal effects ")
   anova(modc,by="margin",perm=500)
   
-  print(" Permutation test - significance of the axis ")
+  print("\n Permutation test - significance of the axis ")
   anova(modc,by="axis",perm=1000)
   sink(NULL)
   
-  pdf(paste(opt$out,"m_CCA.pdf",sep=''),width=8, height=5)
+  pdf(paste(opt$out,"m01_CCA.pdf",sep=''),width=8, height=5)
   plot(modc)
   text(modc,display="sites")
   dev.off()
@@ -229,107 +229,116 @@ if (opt$model==T){
   sink(file=paste(opt$out,"e_Envfitting2CA.txt",sep='')) 
   ef 
   sink(NULL) 
-  pdf(paste(opt$out,"m_EnvCCA.pdf",sep=''),width=8, height=5)
-  plot(ca,display="sites",xlab=paste("CA1:",round(modc$CCA$eig[1]/sum(modc$CCA$eig),2)),
+  pdf(paste(opt$out,"m02_lcCCA.pdf",sep=''),width=8, height=5)
+  plot(modc,display=c("bp","lc"),xlab=paste("CA1:",round(modc$CCA$eig[1]/sum(modc$CCA$eig),2)),
        ylab=paste("CA2:",round(modc$CCA$eig[2]/sum(modc$CCA$eig),2)))
-  plot(modc,display="wa")
   dev.off()
   
   spenvcor(modc)
-  plot(modc,display=c("bp","lc","wa"),type="points")
-  plot(modc,display=c("lc","wa"),type="points")
-  ordispider(modc,col="blue")
+  pdf(paste(opt$out,"m03_waCCA.pdf",sep=''),width=8, height=5)
+  plot(modc,display=c("bp","wa"),xlab=paste("CA1:",round(modc$CCA$eig[1]/sum(modc$CCA$eig),2)),
+       ylab=paste("CA2:",round(modc$CCA$eig[2]/sum(modc$CCA$eig),2)))
+  dev.off()
   
-  pred <- calibrate(modc)
-  head(pred)
-  with(q,plot(Salinity,pred[,"Salinity"]-Salinity,ylab="Prediction Error"))  
-  abline(h=0,col="grey")
+  plot(modc,display="wa")
+  #plot(modc,display=c("bp","lc","wa"),type="points")
+  #plot(modc,display=c("wa"),type="points")
+  #plot(modc,display=c("bp"),type="points")
+  #plot(modc,display=c("lc","wa"),type="points")
+  #plot(modc,display=c("bp","lc"),type="points")
+  #plot(modc,display=c("bp","wa"))
+  #ordispider(modc,col="blue")
   
-  plot(mod=)
+  #pred <- calibrate(modc)
+  #head(pred)
+  #with(q,plot(Salinity,pred[,"Salinity"]-Salinity,ylab="Prediction Error"))  
+  #abline(h=0,col="grey")
+  
+  
 }
-  if(opt$assess_model==''){
-    
-}
+
+## UNDER CONSTRUCTION ## 
+#  if(opt$assess_model==''){
+#    
+#}
 ### constraint analysis
-dfc ~ Salinity + CT + Arcilla + CO + CE + Cu + Limo + pH
+#dfc ~ Salinity + CT + Arcilla + CO + CE + Cu + Limo + pH
 #cca <- cca(dfc ~ Salinity+Fe+CO+CT,x)
 
-cca <- cca(dfc ~ Salinity + CT + Arcilla,x)
-cca
-plot(cca)
-
-
-cca <- cca(dfc ~ Textura,y)
-cca
-plot(cca)
+#cca <- cca(dfc ~ Salinity + CT + Arcilla,x)
+#cca
+#plot(cca)
+#cca <- cca(dfc ~ Textura,y)
+#cca
+#plot(cca)
  
-rda2 <- rda(dfc ~ Salinity + CT + Arcilla,x)
-rda2
-plot(rda2,display="sites")
-plot(rda2)
+#rda2 <- rda(dfc ~ Salinity + CT + Arcilla,x)
+#rda2
+#plot(rda2,display="sites")
+#plot(rda2)
 
 ## permutation test
 
-cca <- cca(dfc ~ Salinity+CT+Arena,x)
-cca
-plot(cca)
-anova(cca,by="term",step=200)
-anova(cca,by="margin",perm=500)
-anova(cca,by="axis",perm=1000)
+#cca <- cca(dfc ~ Salinity+CT+Arena,x)
+#cca
+#plot(cca)
+#anova(cca,by="term",step=200)
+#anova(cca,by="margin",perm=500)
+#anova(cca,by="axis",perm=1000)
 
-mod1 <- cca(dfc ~ .,x)
-mod1
-plot(procrustes(cca(dfc),mod1))
+#mod1 <- cca(dfc ~ .,x)
+#mod1
+#plot(procrustes(cca(dfc),mod1))
 
-mod2 <- cca(dfc ~ Salinity + CT + Arcilla,x)
-mod2
-plot(procrustes(cca(dfc),mod2))
+#mod2 <- cca(dfc ~ Salinity + CT + Arcilla,x)
+#mod2
+#plot(procrustes(cca(dfc),mod2))
 
-mod3 <- cca(dfc ~ Salinity+Condition(Arcilla),x)
-mod3
-anova(mod3,perm.max=2000)
-plot(mod3,display="sites")
-plot(procrustes(cca(dfc),mod3))
+#mod3 <- cca(dfc ~ Salinity+Condition(Arcilla),x)
+#mod3
+#anova(mod3,perm.max=2000)
+#plot(mod3,display="sites")
+#plot(procrustes(cca(dfc),mod3))
 
-ef <- envfit(ca,x,permu=999)
-ef
-plot(ca,display="sites")
-plot(ef,p.max=0.05)
+#ef <- envfit(ca,x,permu=999)
+#ef
+#plot(ca,display="sites")
+#plot(ef,p.max=0.05)#
 
-efc <- envfit(mod2,x,permu=999)
-efc
-plot(mod2)
-plot(ca)
-plot(mod2,display="sites")
-plot(efc,p.max=0.05)
+#efc <- envfit(mod2,x,permu=999)
+#efc
+#plot(mod2)
+#plot(ca)
+#plot(mod2,display="sites")
+#plot(efc,p.max=0.05)
 
-mod0 <- cca(dfc ~ 1,x)
-mod <- step(mod0,scope=formula(mod1),test="perm")
+#mod0 <- cca(dfc ~ 1,x)
+#mod <- step(mod0,scope=formula(mod1),test="perm")
 
-vif.cca(mod1)
-vif.cca(mod)
+#vif.cca(mod1)
+#vif.cca(mod)
 
-ccam <- cca(dfc~ Salinity+CO+Arcilla+CT+CE+Cu+Limo,x)
-ccam
-plot(ccam)
-anova(ccam,by="term",step=200)
-anova(ccam,by="margin",perm=500)
-anova(ccam,by="axis",perm=1000)
+#ccam <- cca(dfc~ Salinity+CO+Arcilla+CT+CE+Cu+Limo,x)
+#ccam
+#plot(ccam)
+#anova(ccam,by="term",step=200)
+#anova(ccam,by="margin",perm=500)
+#anova(ccam,by="axis",perm=1000)
 ## weights
 
-spenvcor(mod)
-spenvcor(mod2)
+#spenvcor(mod)
+#spenvcor(mod2)
 
-k <- cca(dfc ~ Textura,y)
-plot(k,display=c("lc","wa"),type="p")
-ordispider(k,col="blue")
+#k <- cca(dfc ~ Textura,y)
+#plot(k,display=c("lc","wa"),type="p")
+#ordispider(k,col="blue")
 
-with(y,ordiellipse(ca,Textura,kind="se"))
-with(y,ordispider(ca,Textura,col="blue",label=T))
-with(y,ordihull(ca,Textura,col="blue",lty=2))
-plot(ef)
+#with(y,ordiellipse(ca,Textura,kind="se"))
+#with(y,ordispider(ca,Textura,col="blue",label=T))
+#with(y,ordihull(ca,Textura,col="blue",lty=2))
+#plot(ef)
 #
 
-barplot(cca$CCA$eig,border=NA,col="gray80",names=colnames(cca$CCA$eig))
+#barplot(cca$CCA$eig,border=NA,col="gray80",names=colnames(cca$CCA$eig))
 
 
